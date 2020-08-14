d63f959ad0f36810e22a83b5ac8c2537a155a973
jacoblovins <lovins.jacob@yahoo.com>
2020-08-14 14:05:20 -0400

    logging employee information

--- app.js
+++ app.js
@@ -8,9 +8,95 @@ const fs = require("fs");
 const OUTPUT_DIR = path.resolve(__dirname, "output");
 const outputPath = path.join(OUTPUT_DIR, "team.html");
 
-const render = require("./lib/htmlRenderer");
+// const render = require("./lib/htmlRenderer");
+
+const typeQuestion = [
+    {
+        type: "list",
+        message: "What type of employee:",
+        name: "employee",
+        choices: ["Manager", "Engineer", "Intern", "I dont want to add another employee"]
+    }
+]
+
+const generalQuestions = [
+    {
+        type: "input",
+        message: "Employee name:",
+        name: "name"
+    },
+    {
+        type: "input",
+        message: "Employee ID:",
+        name: "id"
+    },
+    {
+        type: "input",
+        message: "Employee Email:",
+        name: "email"
+    },
+]
+
+const managerQuestion = [
+    {
+        type: "input",
+        message: "Office Phone number:",
+        name: "number"
+    }
+]
+
+const engineerQuestion = [
+    {
+        type: "input",
+        message: "GitHub Link:",
+        name: "github"
+    }
+]
+
+const internQuestion = [
+    {
+        type: "input",
+        message: "School:",
+        name: "school"
+    }
+]
 
 
+function getQuestions(typeAnswer){
+
+    if(typeAnswer.employee === "Manager"){
+        return managerQuestion;
+    } else if(typeAnswer.employee === "Engineer") {
+        return engineerQuestion;
+    } else if(typeAnswer.employee === "Intern"){
+        return internQuestion;
+    }
+    init();
+}
+
+async function init(){
+    try{
+        const typeAnswer = await inquirer.prompt(typeQuestion);
+        if(typeAnswer.employee === "I dont want to add another employee"){
+            // render();
+            return
+        } else{
+            const choice = await getQuestions(typeAnswer);
+            const generalAnswers = await inquirer.prompt(generalQuestions);
+            const employeeAnswer = await inquirer.prompt(choice);
+            let employeeDetails = {
+                ...generalAnswers,
+                ...employeeAnswer
+            };
+            console.log(employeeDetails);
+            // .map the employees informatin into a new array
+        }
+
+    } catch (error) {
+        console.log(error);
+    }
+}
+init();
 // Write code to use inquirer to gather information about the development team members,
 // and to create objects for each team member (using the correct classes as blueprints!)
 
