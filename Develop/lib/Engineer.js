9098e799734fe6004f2babef44b0e57ba11e8055
jacoblovins <lovins.jacob@yahoo.com>
2020-08-14 10:52:12 -0400

    added initial classes and templates

---
+++ Develop/lib/Engineer.js
@@ -0,0 +1,15 @@
+const Employee = require("./Employee")
+
+
+// TODO: Write code to define and export the Engineer class.  HINT: This class should inherit from Employee.
+class Engineer extends Employee {
+    constructor(){
+        this.icon = "";
+
+        super()
+        
+    }
+
+}
+
+module.exports = Engineer;
---
+++ Develop/lib/Manager.js
@@ -0,0 +1,15 @@
+const Employee = require("./Employee")
+
+
+// TODO: Write code to define and export the Manager class. HINT: This class should inherit from Employee.
+class Manager extends Employee {
+    constructor(){
+        this.icon = "";
+
+        super()
+        
+    }
+
+}
+
+module.exports = Manager;
