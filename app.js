6b2c6cd839cb1a0277c932832c44896bde9478d6
jacoblovins <lovins.jacob@yahoo.com>
2020-08-20 13:28:07 -0400

    finalized styling

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+// Require all necessary modules
 const Manager = require("./lib/Manager");
 const Engineer = require("./lib/Engineer");
 const Intern = require("./lib/Intern");
@@ -7,12 +8,16 @@ const inquirer = require("inquirer");
 const path = require("path");
 const fs = require("fs");
 
+// Create path to final HTML output
 const OUTPUT_DIR = path.resolve(__dirname, "output");
 const outputPath = path.join(OUTPUT_DIR, "team.html");
 
+// Global variables that will be changed
 let employees = [];
 let person;
 
+// Initial function when the page runs
+// Gets info for the manager and pushes a manager object to employees array
 async function init() {
     console.log("Input manager information:")
     try {
@@ -26,10 +31,13 @@ async function init() {
     }
 }
 
+// Repeating function that asks either engineer or intern questions
+// Creates a new engineer or intern object and pushes it to employees
 async function otheremployees() {
     try {
         const typeAnswer = await inquirer.prompt(questions.typeQuestion);
         if (typeAnswer.employee === "Engineer") {
+            // Engineer questions and actions
             console.log("Input engineer information:")
             const generalAnswers = await inquirer.prompt(questions.generalQuestions);
             const engineerAnswer = await inquirer.prompt(questions.engineerQuestion);
@@ -37,6 +45,7 @@ async function otheremployees() {
             employees.push(person);
             otheremployees()
         } else if (typeAnswer.employee === "Intern") {
+            // Intern questions and actions
             console.log("Input intern information:")
             const generalAnswers = await inquirer.prompt(questions.generalQuestions);
             const internAnswer = await inquirer.prompt(questions.internQuestion);
@@ -44,7 +53,8 @@ async function otheremployees() {
             employees.push(person);
             otheremployees()
         } else if (typeAnswer.employee === "I dont want to add another employee") {
-
+            // Take the compiled information, run it through the render function, and
+            // send the returned data to the team.html file
             fs.writeFile(outputPath, render(employees), function (err) {
                 if (err) {
                     return console.log(err);
